b01f83e21f73daa950ab97a27783067555339f60
donker008 <richdonker@qq.com>
2017-03-10 20:24:25 +0800

    implement create post & add missing segment: group_id, user_id to post table

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -8,10 +8,20 @@ end
 def new
   @group = Group.find(params[:group_id])
   @post = Post.new
-  @post.group = @group
 end
 
 def create
+
+ @group = Group.find(params[:group_id])
+ @post = Post.new(post_params)
+ @post.group = @group
+ @post.user = current_user
+ if @post.save
+   flash[:notice] = "Post success!"
+   redirect_to group_path(@group)
+ else
+   render :new
+ end
 
 end
 
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170310110237) do
+ActiveRecord::Schema.define(version: 20170310121934) do
 
   create_table "groups", force: :cascade do |t|
     t.string   "title"
@@ -24,6 +24,8 @@ ActiveRecord::Schema.define(version: 20170310110237) do
     t.text     "content"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer  "group_id"
+    t.integer  "user_id"
   end
 
   create_table "users", force: :cascade do |t|
