31d494eae16d29f4852f056abbd09f98277443c5
donker008 <richdonker@qq.com>
2017-03-10 16:34:39 +0800

    add group and implement index,edit,delete

---
+++ app/controllers/groups_controller.rb
@@ -0,0 +1,17 @@
+class GroupsController < ApplicationController
+
+  def index
+    @groups = Group.all
+  end
+
+  def edit
+    @group = Group.find(params[:id])
+  end
+
+  def destroy
+    @group = Group.find(params[:id])
+    @group.destroy
+    redirect_to groups_path
+  end
+
+end
