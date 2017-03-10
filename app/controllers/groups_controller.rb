f1e8258c6c3fa063c510d4352d9e489e042a571d
donker008 <richdonker@qq.com>
2017-03-10 17:34:11 +0800

    implement edit,create group functions

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -11,7 +11,34 @@ class GroupsController < ApplicationController
   def destroy
     @group = Group.find(params[:id])
     @group.destroy
+    flash[:alert] = "Group deleted!"
     redirect_to groups_path
   end
 
+  def new
+    @group = Group.new
+  end
+
+  def create
+    @group = Group.new(group_params)
+    @group.save
+    flash[:notice] = "Group created!"
+    redirect_to groups_path
+  end
+
+  def update
+    @group = Group.find(params[:id])
+
+    if @group.update(group_params)
+      flash[:notice] = "Group updated success!"
+    else
+      flash[:warning] = "Group updated failed!"
+    end
+    redirect_to groups_path
+  end
+
+  private
+  def group_params
+    params.require(:group).permit(:title, :description)
+  end
 end
